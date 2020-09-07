caa4e33e5192552ecb395716dd4836b8b775d12a
juan <juancraft12yt@gmail.com>
2020-09-06 22:06:37 -0300

    actualiza clase equipo
    actualiza clase equipo para agregar direccion de los estadios

--- src/entities/equipo.js
+++ src/entities/equipo.js
@@ -1,13 +1,14 @@
 module.exports = class Equipo{
-    constructor(nombre, abreviatura, estadio, telefono, sitioWeb, fotoEscudo, pais, anoFundacion, numeroId){
+    constructor(nombre, abreviatura, estadio, direccion, anoFundacion, numeroId, telefono, website, pais, fotoEscudo){
         this.nombre = nombre
         this.abreviatura = abreviatura 
         this.estadio = estadio
-        this.telefono = telefono
-        this.sitioWeb = sitioWeb
-        this.fotoEscudo = fotoEscudo
-        this.pais = pais
+        this.direccion = direccion
         this.anoFundacion = anoFundacion
         this.numeroId = numeroId
+        this.telefono = telefono
+        this.website = website
+        this.pais = pais
+        this.fotoEscudo = fotoEscudo
     }
 }
