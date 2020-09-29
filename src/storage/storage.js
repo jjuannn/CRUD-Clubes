a80a4d317747c34f2240daf953e0c73d4e99688e
juan <juancraft12yt@gmail.com>
2020-09-28 22:32:15 -0300

    agrega funciones
    agrega nuevas funciones para guardar equipos

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -3,20 +3,8 @@ const fs = require ("fs")
 const mapper = require("../mapper/dataMapper.js")
 const dataMapper = mapper.dataMapper
 
-function obtenerTodosLosEquipos(){
-    return leerListaDeEquipos()
-}
-
-function leerListaDeEquipos(){
-    const clubes = JSON.parse(fs.readFileSync("./data/listaEquipos.json", "utf-8"))
-    const listaEquipos = clubes.map( club => {
-        return dataMapper(club)
-    })
-    return listaEquipos
-}
-
 function obtenerPorId(idEquipo){
-    const clubes = leerListaDeEquipos()
+    const clubes = obtenerTodosLosEquipos()
     let equipoSeleccionado
     for(let i = 0; i < clubes.length; i++){
         if(Number(clubes[i].numeroId) === Number(idEquipo)){
@@ -26,7 +14,26 @@ function obtenerPorId(idEquipo){
     return equipoSeleccionado
 }
 
+function guardarEquipo(nuevoEquipo){
+    const equipos = obtenerTodosLosEquipos()
+
+    equipos.push(nuevoEquipo)
+    fs.writeFileSync("./data/listaEquipos.json", JSON.stringify(equipos))
+
+    return dataMapper(nuevoEquipo)
+}
+
+
+function obtenerTodosLosEquipos(){
+    const clubes = JSON.parse(fs.readFileSync("./data/listaEquipos.json", "utf-8"))
+    const listaEquipos = clubes.map( club => {
+        return dataMapper(club)
+    })
+    return listaEquipos
+}
+
 module.exports = {
     obtenerTodosLosEquipos,
-    obtenerPorId
+    obtenerPorId,
+    guardarEquipo
 }
