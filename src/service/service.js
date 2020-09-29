38fbfddd6cc37a14c6a924e666ce8a9a26a592c0
juan <juancraft12yt@gmail.com>
2020-09-28 22:32:54 -0300

    agrega funcion para crear un equipo
    agrega funcion para crear un equipo, llamando a guardar equipo del modulo storage. Asi, el controlador no se comunica directamente con la capa de storage

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,4 @@
+const { resolveAny, resolveSoa } = require("dns")
 const fs = require("fs")
 
 const storage = require("../storage/storage.js")
@@ -10,7 +11,20 @@ function obtenerPorId(numeroId){
     return storage.obtenerPorId(numeroId)
 }
 
+function crearEquipo(nuevoEquipo){
+    const equipos = storage.obtenerTodosLosEquipos()
+
+    for(let i = 0; i < equipos.length; i++){
+        if(equipos[i].numeroId === nuevoEquipo.numeroId ){
+            throw new Error("El ID que ingresaste ya esta en uso. Por favor introducir uno nuevo")
+        }
+    }
+    return storage.guardarEquipo(nuevoEquipo)
+}
+
+
 module.exports = {
     obtenerTodosLosEquipos,
-    obtenerPorId
+    obtenerPorId,
+    crearEquipo
 }
