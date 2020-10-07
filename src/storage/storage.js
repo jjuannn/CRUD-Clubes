bebb90eaba746b7b4bcad21ce0937217a936dd8b
juan <juancraft12yt@gmail.com>
2020-10-06 23:04:27 -0300

    Agrega funciones de storage
    Agrega funciones de almacenamiento faltantes para la separacion en modulos correcta.

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -4,20 +4,38 @@ const mapper = require("../mapper/dataMapper.js")
 const dataMapper = mapper.dataMapper
 
 function obtenerTodosLosEquipos(){
-    return leerListaDeEquipos()
-}
-
-function leerListaDeEquipos(){
     const clubes = JSON.parse(fs.readFileSync("./data/listaEquipos.json", "utf-8"))
-    const listaEquipos = clubes.map( club => {
-        return dataMapper(club)
-    })
+    const listaEquipos = clubes.map(club => dataMapper(club));
     return listaEquipos
 }
 
+function sobreescribirDB(info){
+    fs.writeFileSync("./data/listaEquipos.json", JSON.stringify(info))
+}
+
+function guardarEquipoNuevo(equipoNuevo){
+    const equipos = obtenerTodosLosEquipos()
+
+    const valorActual = equipos.length
+
+    for(let i = 0; i < equipos.length; i++){
+        if(equipos[i].numeroId === equipoNuevo.numeroId ){
+            throw new Error("El ID que ingresaste ya esta en uso. Por favor introducir uno nuevo")
+        }
+    }
+
+    equipos.push(equipoNuevo)
+    
+    sobreescribirDB(equipos)
+    
+    return equipos.length === valorActual
+}
+
 function obtenerPorId(idEquipo){
-    const clubes = leerListaDeEquipos()
+    const clubes = obtenerTodosLosEquipos()
+
     let equipoSeleccionado
+
     for(let i = 0; i < clubes.length; i++){
         if(Number(clubes[i].numeroId) === Number(idEquipo)){
             equipoSeleccionado = clubes[i]
@@ -26,7 +44,40 @@ function obtenerPorId(idEquipo){
     return equipoSeleccionado
 }
 
+function borrarEquipo(id){
+    const equipos = obtenerTodosLosEquipos()
+
+    for(let i = 0; i < equipos.length; i++){
+        if(Number(equipos[i].numeroId) === Number(id)){
+            equipos.splice(i, 1)
+            break
+        }
+    }
+
+    sobreescribirDB(equipos)
+}
+
+function guardarCambiosEquipo(equipoEditado){
+    const equipos = obtenerTodosLosEquipos()
+
+    for(let i = 0; i < equipos.length; i++){
+        if(Number(equipoEditado.numeroId) === Number(equipos[i].numeroId)){
+            if(!equipoEditado.fotoEscudo){ 
+                equipoEditado.fotoEscudo = equipos[i].fotoEscudo
+            }
+            equipos.splice(i, 1, equipoEditado)
+        }
+    }
+
+    sobreescribirDB(equipos)
+
+    return equipoEditado
+}
+
 module.exports = {
     obtenerTodosLosEquipos,
-    obtenerPorId
+    obtenerPorId,
+    guardarEquipoNuevo,
+    borrarEquipo,
+    guardarCambiosEquipo
 }
